eworm-de/routeros-scripts

commit 1e8918fdaa5a30393e2004d1f5e4dff458936b67

global-functions: $ScriptLock: do not exit from global function

global-functions.rsc
@@ -1170,8 +1170,7 @@
 # lock script against multiple invocation
 :set ScriptLock do={
   :local Script   [ :tostr $1 ];
-  :local DoReturn $2;
-  :local WaitMax  ([ :tonum $3 ] * 10);
+  :local WaitMax ([ :tonum $3 ] * 10);
 
   :global GetRandom20CharAlNum;
   :global IfThenElse;
@@ -1286,13 +1285,13 @@
   :if ([ $IsFirstTicket $Script $MyTicket ] = true && [ $TicketCount $Script ] = [ $JobCount $Script ]) do={
     $RemoveTicket $Script $MyTicket;
     $CleanupTickets $Script;
-    :return false;
+    :return true;
   }
 
   $RemoveTicket $Script $MyTicket;
   $LogPrintExit2 info $0 ("Script '" . $Script . "' started more than once" . [ $IfThenElse ($WaitCount > 0) \
-    " and timed out waiting for lock" "" ] . "... Aborting.") [ $IfThenElse ($DoReturn = true) false true ];
-  :return true;
+    " and timed out waiting for lock" "" ] . "... Aborting.") false;
+  :return false;
 }
 
 # send notification via NotificationFunctions - expects at least two string arguments

backup-cloud.rsc
@@ -30,7 +30,9 @@
   :global SymbolForNotification;
   :global WaitFullyConnected;
 
-  $ScriptLock $ScriptName;
+  :if ([ $ScriptLock $ScriptName ] = false) do={
+    :return false;
+  }
   $WaitFullyConnected;
 
   :if ([ $ScriptFromTerminal $ScriptName ] = false && $BackupRandomDelay > 0) do={

backup-email.rsc
@@ -45,7 +45,9 @@
     $LogPrintExit2 error $ScriptName ("Configured to send neither backup nor config export.") true;
   }
 
-  $ScriptLock $ScriptName;
+  :if ([ $ScriptLock $ScriptName ] = false) do={
+    :return false;
+  }
   $WaitFullyConnected;
 
   :if ([ $ScriptFromTerminal $ScriptName ] = false && $BackupRandomDelay > 0) do={

backup-partition.rsc
@@ -18,7 +18,9 @@
   :global LogPrintExit2;
   :global ScriptLock;
 
-  $ScriptLock $ScriptName;
+  :if ([ $ScriptLock $ScriptName ] = false) do={
+    :return false;
+  }
 
   :if ([ :len [ /partitions/find ] ] < 2) do={
     $LogPrintExit2 error $ScriptName ("Device does not have a fallback partition.") true;

backup-upload.rsc
@@ -44,7 +44,9 @@
     $LogPrintExit2 error $ScriptName ("Configured to send neither backup nor config export.") true;
   }
 
-  $ScriptLock $ScriptName;
+  :if ([ $ScriptLock $ScriptName ] = false) do={
+    :return false;
+  }
   $WaitFullyConnected;
 
   :if ([ $ScriptFromTerminal $ScriptName ] = false && $BackupRandomDelay > 0) do={

capsman-download-packages.capsman.rsc
@@ -24,7 +24,9 @@
   :global ScriptLock;
   :global WaitFullyConnected;
 
-  $ScriptLock $ScriptName;
+  :if ([ $ScriptLock $ScriptName ] = false) do={
+    :return false;
+  }
   $WaitFullyConnected;
 
   :local PackagePath [ $CleanFilePath [ /caps-man/manager/get package-path ] ];

capsman-download-packages.template.rsc
@@ -25,7 +25,9 @@
   :global ScriptLock;
   :global WaitFullyConnected;
 
-  $ScriptLock $ScriptName;
+  :if ([ $ScriptLock $ScriptName ] = false) do={
+    :return false;
+  }
   $WaitFullyConnected;
 
   :local PackagePath [ $CleanFilePath [ /caps-man/manager/get package-path ] ];

capsman-download-packages.wifi.rsc
@@ -24,7 +24,9 @@
   :global ScriptLock;
   :global WaitFullyConnected;
 
-  $ScriptLock $ScriptName;
+  :if ([ $ScriptLock $ScriptName ] = false) do={
+    :return false;
+  }
   $WaitFullyConnected;
 
   :local PackagePath [ $CleanFilePath [ /interface/wifi/capsman/get package-path ] ];

commit 27987a0d7cf54343aa15ee9dd23f7006b323bf2a

global-functions: $ScriptLock: fix second parameter
This broke with 1e8918fdaa5a30393e2004d1f5e4dff458936b67...

Fixes: https://github.com/eworm-de/routeros-scripts/issues/95

global-functions.rsc
@@ -1388,7 +1388,7 @@
 # lock script against multiple invocation
 :set ScriptLock do={
   :local Script   [ :tostr $1 ];
-  :local WaitMax ([ :tonum $3 ] * 10);
+  :local WaitMax ([ :tonum $2 ] * 10);
 
   :global GetRandom20CharAlNum;
   :global IfThenElse;