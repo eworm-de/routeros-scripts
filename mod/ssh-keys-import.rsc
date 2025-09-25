eworm-de/routeros-scripts

commit 80aed200fd7400e4a4958ba314912488780be635

mod/ssh-keys-import: use $FileExists ...
... to work around restrictions in new file handling.

mod/ssh-keys-import.rsc
@@ -75,6 +75,7 @@
   :local User     [ :tostr $2 ];
 
   :global EitherOr;
+  :global FileExists;
   :global LogPrint;
   :global ParseKeyValueStore;
   :global SSHKeysImport;
@@ -84,8 +85,7 @@
     :return false;
   }
 
-  :local File [ /file/find where name=$FileName ];
-  :if ([ :len $File ] = 0) do={
+  :if ([ $FileExists $FileName ] = true) do={
     $LogPrint warning $0 ("File '" . $FileName . "' does not exist.");
     :return false;
   }

commit 2c715096b994811fbc914f68412db91f5bd62618

mod/ssh-keys-import: $SSHKeysImportFile: fix file exists condition...
... which broke in commit 80aed200fd7400e4a4958ba314912488780be635.

mod/ssh-keys-import.rsc
@@ -85,7 +85,7 @@
     :return false;
   }
 
-  :if ([ $FileExists $FileName ] = true) do={
+  :if ([ $FileExists $FileName ] = false) do={
     $LogPrint warning $0 ("File '" . $FileName . "' does not exist.");
     :return false;
   }