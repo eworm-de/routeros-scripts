eworm-de/routeros-scripts

commit 6b0c9ab17d2e7e3693cebf281e5300d1e4de3446

packages-update: exit early if scheduler exists

packages-update.rsc
@@ -41,6 +41,11 @@
       /system/reboot;
     }
 
+    :if ([ /system/scheduler/find where name="_RebootForUpdate" ] > 0) do={
+      $LogPrint warning $ScriptName ("Scheduler for reboot already exists.");
+      :return false;
+    }
+
     :local Interval [ $IfThenElse ([ :totime $PackagesUpdateDeferReboot ] >= 1d) \
         $PackagesUpdateDeferReboot 1d ];
     :local StartTime [ :tostr [ :totime (10800 + [ $GetRandomNumber 7200 ]) ] ];

commit a931cb61a033949a9cc6a1b62bc1e267774b04a6

packages-update: fix condition when checking for scheduler
This broke with commit 6b0c9ab17d2e7e3693cebf281e5300d1e4de3446...

packages-update.rsc
@@ -41,7 +41,7 @@
       /system/reboot;
     }
 
-    :if ([ /system/scheduler/find where name="_RebootForUpdate" ] > 0) do={
+    :if ([ :len [ /system/scheduler/find where name="_RebootForUpdate" ] ] > 0) do={
       $LogPrint warning $ScriptName ("Scheduler for reboot already exists.");
       :return false;
     }