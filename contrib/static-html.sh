e0b12a9050eeb1c231ace3b5b5186b34514e563d
Christian Hesse <mail@eworm.de>
2025-11-18 19:01:39 +0100

    contrib/static-html: add badges

--- contrib/static-html.sh
+++ contrib/static-html.sh
@@ -5,4 +5,6 @@ set -e
 sed -i \
 	-e '/href=/s|\.md|\.html|' \
 	-e '/blockquote/s|/\* display \*/|display: none;|' \
+	-e '/<!-- badges here \/\/-->/r badges.html' \
+	-e '/<!-- badges here \/\/-->/d' \
 	"${@}"
