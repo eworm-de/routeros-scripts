4f778cb1c608e384bf75de267bdf9b7ea1facf00
Christian Hesse <mail@eworm.de>
2025-11-19 16:38:27 +0100

    general/clipboard: add a visual feedback

--- general/clipboard.js
+++ general/clipboard.js
@@ -1,4 +1,8 @@
 /* copy code to clipboard */
 function CopyToClipboard(element) {
+  element.style.filter = 'invert(1)';
   navigator.clipboard.writeText(element.firstElementChild.innerHTML);
+  setTimeout(function() {
+    element.style.filter = 'invert(0)';
+  }, 100);
 }
