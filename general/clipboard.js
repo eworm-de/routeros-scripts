699be25b64a8b67a99786387c8b0407f7e939590
Christian Hesse <mail@eworm.de>
2025-11-20 17:05:46 +0100

    general/clipboard: use textContent...
    ... and avoid having html entity encoded characters in clipboard.

--- general/clipboard.js
+++ general/clipboard.js
@@ -1,7 +1,7 @@
 /* copy code to clipboard */
 function CopyToClipboard(element) {
   element.style.filter = 'invert(1)';
-  navigator.clipboard.writeText(element.firstElementChild.innerHTML);
+  navigator.clipboard.writeText(element.firstElementChild.textContent);
   setTimeout(function() {
     element.style.filter = 'invert(0)';
   }, 100);
