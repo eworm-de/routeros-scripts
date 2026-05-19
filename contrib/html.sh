0e5c07e34484d29234a0ef7d59b24e818a1722f0
Christian Hesse <mail@eworm.de>
2026-05-19 15:11:41 +0200

    contrib/html: support any class name from image alt text

--- contrib/html.sh
+++ contrib/html.sh
@@ -17,7 +17,7 @@ markdown -f toc,idanchor "${1}" | sed \
 	-e '/<h[1234] /s|-2[1789cd]-||g' \
 	-e '/<h[1234] /s|-3[f]-||g' \
 	-e '/^<pre>/s|pre|pre class="code" onclick="CopyToClipboard(this)"|g' \
-	-e '/<img src=".*" alt="\(notification\|screenshot\): .*" \/>/s|alt="\(.*\):|class="\1" alt="\1:|' \
+	-e '/<img src=".*" alt="[a-z]\+: .*" \/>/s|alt="\([a-z]\+\):|class="\1" alt="\1:|' \
 	-e '/The above link may be broken on code hosting sites/s|blockquote|blockquote style="display: none;"|'
 
 sed \
